57788fa84f43f1a9515196a05278e997086d8869
Jayfend <1951120108@sv.ut.edu.vn>
2022-03-23 15:30:13 +0700

    Add project files.

---
+++ CoffeShop/Controllers/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeShop.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            ViewBag.Menu = "active";
+            return View();
+        }
+    }
+}
---
+++ CoffeShop/Controllers/TodaySpecialsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeShop.Controllers
+{
+    public class TodaySpecialsController : Controller
+    {
+        // GET: TodaySpecials
+        public ActionResult Index()
+        {
+            ViewBag.TodaySpecials = "active";
+            return View();
+        }
+    }
+}
